7900738372cd2cd1dd46725e8ba4e5111af677ec
nikokip20 <116296978+nikokip20@users.noreply.github.com>
2022-11-25 08:29:33 +0200

    updating the JASON file
    extending the array

--- subject.js
+++ subject.js
@@ -1,28 +1,94 @@
-let products =[
+let products = [
         {
-        "productID": 1001,
-         "Subject": "Math",
-        "Location": "London",
-         "Price": 100,
-        "Space": 5,
-         "image": "Subjects//math.png"
+                "productID": 1001,
+                "Subject": "Math",
+                "Location": "London",
+                "Price": 100,
+                "Space": 5,
+                "image": "Subjects//math.png"
         },
 
         {
-        "productID": 1021,      
-        "Subject": "Law",
-        "Location": "Brighton",
-         "Price": 200,
-        "Space": 6,
-         "image": "Subjects//Law.png"
+                "productID": 1021,
+                "Subject": "Law",
+                "Location": "Brighton",
+                "Price": 200,
+                "Space": 6,
+                "image": "Subjects//Law.jpg"
         },
 
         {
-        "productID": 301,
-         "Subject": "French",
-        "Location": "Berlin",
-         "Price": 300,
-        "Space": 8,
-         "image": "Subjects//French.png"
-        }
+                "productID": 11,
+                "Subject": "Physics",
+                "Location": "Berlin",
+                "Price": 300,
+                "Space": 8,
+                "image": "Subjects//Physics.jpg"
+        },
+        {
+                "productID": 1001,
+                "Subject": "Geography",
+                "Location": "London",
+                "Price": 100,
+                "Space": 5,
+                "image": "Subjects//geograpthy.jpg"
+        },
+
+        {
+                "productID": 191,
+                "Subject": "Literature",
+                "Location": "Brighton",
+                "Price": 200,
+                "Space": 12,
+                "image": "Subjects//Literature.jpg"
+        },
+
+        {
+                "productID": 301,
+                "Subject": "computer sience",
+                "Location": "Oxford",
+                "Price": 150,
+                "Space": 8,
+                "image": "Subjects//computer sience.jpg"
+        },
+        {
+                "productID": 582,
+                "Subject": "Chemestry",
+                "Location": "Wales",
+                "Price": 600,
+                "Space": 5,
+                "image": "Subjects//Chemestry.jpg"
+        },
+
+        {
+                "productID": 1081,
+                "Subject": "History",
+                "Location": "Paris",
+                "Price": 200,
+                "Space": 3,
+                "image": "Subjects//History.jpg"
+        },
+
+
+        {
+                "productID": 1001,
+                "Subject": "BS",
+                "Location": "London",
+                "Price": 480,
+                "Space": 5,
+                "image": "Subjects//business management.jpg"
+        },
+
+        {
+                "productID": 1021,
+                "Subject": "Economics",
+                "Location": "Brighton",
+                "Price": 400,
+                "Space": 8,
+                "image": "Subjects//Economics.jpg"
+        },
+        
+
+
+
 ]
